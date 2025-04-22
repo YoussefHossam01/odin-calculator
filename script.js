eab61a6960f15bbf397533bfbfb7b2cd1939c11a
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2025-04-22 22:32:24 +0200

    Add operate function

--- script.js
+++ script.js
@@ -6,18 +6,36 @@ let operator;
 let num2;
 
 function add(a, b){
-    return a + b;
-    
+    return a + b;  
 }
+
 function subtract(a, b){
     return a - b;
 }
+
 function multiply(a, b){
     return a * b;
 }
+
 function divide(a, b){
-    return b !== 0? a / b : Nan;
+    return b !== 0? a / b : NaN;
 }
+
+function operate(a,operation,b){
+    if (operation === '+'){
+        return add(a,b);
+    }
+    else if (operation === '-'){
+        return subtract(a,b);
+    }
+    else if (operation === '*'){
+        return multiply(a,b);
+    }
+    else if (operation === '/'){
+        return divide(a,b);
+    }
+}
+
 nine.addEventListener("click", () => {
     display.innerHTML = "9";
 })
